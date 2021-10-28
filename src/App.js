6f40d59ea923d5ac1b13c1e5f1fc671ab8eefb9d
unknown <lpsk5713@gmail.com>
2021-10-29 02:33:44 +0530

    Basics of Props Added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,38 @@
+import ExpenseItem from "./components/ExpenseItem";
+
 function App() {
+
+  const expenses = [
+    { id: "e1", title: "Car Insurance", amount: 41, date: new Date(2020, 5, 2) },
+    { id: "e2", title: "Bike Insurance", amount: 14, date: new Date(2020, 3, 7) },
+    { id: "e3", title: "Monitor Buy", amount: 64, date: new Date(2020, 8, 7) },
+    { id: "e4", title: "Stand Buy", amount: 74, date: new Date(2020, 5, 7) },
+  ]
+
   return (
     <div>
       <h2>Let's get started!</h2>
-    </div>
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}>
+      </ExpenseItem>
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}>
+      </ExpenseItem>
+      <ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}>
+      </ExpenseItem>
+      <ExpenseItem
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+        date={expenses[3].date}>
+      </ExpenseItem>
+    </div >
   );
 }
 
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,25 @@
+import './ExpenseItem.css';
+
+function ExpenseItem(props) {
+    const month = props.date.toLocaleString('en-US', { month: 'long' })
+    const day = props.date.toLocaleString('en-US', { day: '2-digit' })
+    const year = props.date.getFullYear();
+
+    return (
+        <div className="expense-item">
+
+            <div>
+                <div>{month}</div>
+                <div>{year}</div>
+                <div>{day}</div>
+            </div>
+
+            <div className="expense-item__description">
+                <h1>{props.title}</h1>
+                <div className="expense-item__price">{props.amount}</div>
+            </div>
+        </div>
+    )
+}
+
+export default ExpenseItem
