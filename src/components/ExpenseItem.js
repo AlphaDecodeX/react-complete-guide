701bb4965ef3399dcaaba2356cd5c8e9ae52efdc
unknown <lpsk5713@gmail.com>
2021-10-30 02:35:48 +0530

    Props managed

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,18 +1,11 @@
 import './ExpenseItem.css';
+import ExpenseDate from './ExpenseDate';
 
 function ExpenseItem(props) {
-    const month = props.date.toLocaleString('en-US', { month: 'long' })
-    const day = props.date.toLocaleString('en-US', { day: '2-digit' })
-    const year = props.date.getFullYear();
 
     return (
         <div className="expense-item">
-            <div>
-                <div>{month}</div>
-                <div>{year}</div>
-                <div>{day}</div>
-            </div>
-
+            <ExpenseDate date={props.date} />
             <div className="expense-item__description">
                 <h1>{props.title}</h1>
                 <div className="expense-item__price">{props.amount}</div>
