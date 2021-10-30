bc7c2026b5fd7c762988de1f3ae432d2bf4387f4
unknown <lpsk5713@gmail.com>
2021-10-31 02:53:22 +0530

    New components structuring done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 function App() {
   const expenses = [
@@ -11,6 +12,7 @@ function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
+      <NewExpense />
       <Expenses items={expenses} />
     </div >
   );
