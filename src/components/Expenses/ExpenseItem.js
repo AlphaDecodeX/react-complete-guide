0d73ea33c64e967ae3774cd0095de7efb5ef5285
unknown <lpsk5713@gmail.com>
2021-10-31 02:39:57 +0530

    States Introduced with const value too

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,16 +1,24 @@
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
+import { useState } from 'react';
 
 function ExpenseItem(props) {
+    const [title, setTitle] = useState(props.title);
 
+    const clickHandler = () => {
+        setTitle("Updated");
+    }
     return (
         <Card className="expense-item">
             <ExpenseDate date={props.date} />
             <div className="expense-item__description">
-                <h1>{props.title}</h1>
+                <h1>{title}</h1>
                 <div className="expense-item__price">{props.amount}</div>
             </div>
+            <button onClick={clickHandler}>
+                Change Title
+            </button>
         </Card >
     )
 }
