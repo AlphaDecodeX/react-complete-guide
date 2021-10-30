836596620ef2bad1b60f1024cbb4909d2be8f7c4
unknown <lpsk5713@gmail.com>
2021-10-31 01:17:43 +0530

    All files organized

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,16 +1,17 @@
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
+import Card from '../UI/Card';
 
 function ExpenseItem(props) {
 
     return (
-        <div className="expense-item">
+        <Card className="expense-item">
             <ExpenseDate date={props.date} />
             <div className="expense-item__description">
                 <h1>{props.title}</h1>
                 <div className="expense-item__price">{props.amount}</div>
             </div>
-        </div>
+        </Card >
     )
 }
 
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,10 +1,11 @@
 import ExpenseItem from "./ExpenseItem";
 import './Expenses.css';
+import Card from '../UI/Card'
 
 function Expenses(props) {
 
     return (
-        <div className='expenses'>
+        <Card className='expenses'>
             <ExpenseItem
                 title={props.items[0].title}
                 amount={props.items[0].amount}
@@ -25,7 +26,7 @@ function Expenses(props) {
                 amount={props.items[3].amount}
                 date={props.items[3].date}>
             </ExpenseItem>
-        </div>
+        </Card>
     )
 }
 
