2c309317c526576f3d4cb8e4457939b9ae41b0eb
unknown <lpsk5713@gmail.com>
2021-11-01 03:37:38 +0530

    CSS inline styling with JavaScript Conditional Formatting

--- Project 2/src/components/CourseGoals/CourseInput/CourseInput.js
+++ Project 2/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -5,21 +5,35 @@ import './CourseInput.css';
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
+  const [isValid, setIsValid] = useState(true);
 
   const goalInputChangeHandler = event => {
+    if (event.target.value.trim().length > 0) {
+      setIsValid(true);
+    }
     setEnteredValue(event.target.value);
   };
 
   const formSubmitHandler = event => {
     event.preventDefault();
+    if (enteredValue.trim().length === 0) {
+      setIsValid(false);
+      return;
+    }
     props.onAddGoal(enteredValue);
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
       <div className="form-control">
-        <label>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
+        <label style={{
+          color: !isValid ? "red" : "black"
+
+        }}>Course Goal</label>
+        <input style={{
+          borderColor: !isValid ? "red" : "black",
+          background: !isValid ? "salmon" : "transparent"
+        }} type="text" onChange={goalInputChangeHandler} />
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
