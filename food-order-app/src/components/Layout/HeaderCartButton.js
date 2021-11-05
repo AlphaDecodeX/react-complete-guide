2f8cedfed8f11f7d0f58bb706866eaa2b400652c
unknown <lpsk5713@gmail.com>
2021-11-06 01:26:08 +0530

    All components are modified to Handle Cart state

--- food-order-app/src/components/Layout/HeaderCartButton.js
+++ food-order-app/src/components/Layout/HeaderCartButton.js
@@ -1,13 +1,22 @@
 import React from 'react'
 import CartIcon from "../Cart/CartIcon"
 import classes from "./HeaderCartButton.module.css"
+import { useContext } from 'react'
+import CartContext from '../../store/cart-context'
 
 function HeaderCartButton(props) {
+    const cartCtx = useContext(CartContext);
+    const numberOfCartItems = cartCtx.items.reduce(
+        (curNumber, item) => {
+            return curNumber + item.amount;
+        }, 0
+    );
+
     return (
         <button className={classes.button} onClick={props.onClick}>
             <span>Your Cart</span>
             <span className={classes.badge}>
-                3
+                {numberOfCartItems}
             </span>
         </button>
     )
--- food-order-app/src/components/Meals/MealItem/MealItemForm.js
+++ food-order-app/src/components/Meals/MealItem/MealItemForm.js
@@ -1,17 +1,38 @@
 import classes from "./MealItemForm.module.css";
 import Input from "../../UI/Input";
+import { useRef, useState } from "react";
 
 const MealItemForm = (props) => {
-    return <form className={classes.form}>
-        <Input label="Amount" input={{
-            id: 'amount' + props.id,
-            type: 'number',
-            min: '1',
-            max: '5',
-            step: '1',
-            defaultValue: '1'
-        }} />
+    const amountInputRef = useRef();
+    const [amountIsValid, setAmountIsValid] = useState(true);
+
+    const submitHandler = event => {
+        event.preventDefault();
+        const enteredAmount = amountInputRef.current.value;
+        const enteredAmountNumber = +enteredAmount;
+        if (enteredAmount.treim().length === 0 ||
+            enteredAmount < 1 || enteredAmount > 5
+        ) {
+            setAmountIsValid(false);
+            return;
+        }
+
+        props.onAddToCart(enteredAmountNumber);
+    }
+
+    return <form className={classes.form} onSubmit={submitHandler}>
+        <Input label="Amount"
+            ref={amountInputRef}
+            input={{
+                id: 'amount' + props.id,
+                type: 'number',
+                min: '1',
+                max: '5',
+                step: '1',
+                defaultValue: '1'
+            }} />
         <button>+ Add</button>
+        {!amountIsValid && <p>Please enter a valid amount</p>}
     </form>
 }
 
