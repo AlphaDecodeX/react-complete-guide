db026250f34dbb8a8e716942beba8dfd50fe07aa
unknown <lpsk5713@gmail.com>
2021-11-06 00:36:22 +0530

    ContextAPI Provider setuped for Cart

--- food-order-app/src/App.js
+++ food-order-app/src/App.js
@@ -3,6 +3,7 @@ import Cart from "./components/Cart/Cart";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import { useState } from "react";
+import CartProvider from "./store/CartProvider";
 
 function App() {
   const [cartIsShown, setCartIsShown] = useState(false);
@@ -16,13 +17,13 @@ function App() {
   }
 
   return (
-    <React.Fragment>
+    <CartProvider>
       {cartIsShown && <Cart onClose={hideCartHandler} />}
       <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
-    </React.Fragment>
+    </CartProvider>
   );
 }
 
---
+++ food-order-app/src/store/CartProvider.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import CartContext from './cart-context'
+
+function CartProvider(props) {
+    const addItemToCartHandler = item => { };
+
+    const removeItemFromCartHandler = id => { };
+
+    const cartContext = {
+        items: [],
+        totalAmount: 0,
+        addItem: addItemToCartHandler,
+        removeItem: removeItemFromCartHandler
+    };
+
+    return (
+        <CartContext.Provider value={cartContext}>
+            {props.children}
+        </CartContext.Provider>
+    )
+}
+
+export default CartProvider
