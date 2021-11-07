e74aa3adccaef85d9306f702e60208868ef9e12d
unknown <lpsk5713@gmail.com>
2021-11-07 21:59:13 +0530

    useCallback() Dependencies used for change in variable

--- project-6/src/App.js
+++ project-6/src/App.js
@@ -6,14 +6,23 @@ import Demo from './components/Demo/Demo';
 
 function App() {
   const [showparagraph, setShowParagraph] = useState(false);
+  const [allowToggle, setAllowToggle] = useState(false);
+
   const toggleParagraphHandler = useCallback(() => {
-    setShowParagraph(prevShowParagraph => !prevShowParagraph);
-  }, []);
+    if (allowToggle) {
+      setShowParagraph(prevShowParagraph => !prevShowParagraph);
+    }
+  }, [allowToggle]);
+
+  const allowToggleHandler = () => {
+    setAllowToggle(true);
+  }
 
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      <Demo show={false} />
+      <Demo show={showparagraph} />
+      <Button onClick={allowToggleHandler}>Allow Toggling</Button>
       <Button onClick={toggleParagraphHandler}>Toggle Paragraph</Button>
     </div>
   );
