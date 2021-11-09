7d2bf79520f953b810a7850b8ce65953fc369053
unknown <lpsk5713@gmail.com>
2021-11-09 23:04:48 +0530

    Custom Hooks completed

--- project-10/src/App.js
+++ project-10/src/App.js
@@ -1,25 +1,25 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import useHttp from './hooks/use-http';
 import Tasks from './components/Tasks/Tasks';
 import NewTask from './components/NewTask/NewTask';
 
 function App() {
   const [tasks, setTasks] = useState([]);
-  const transformTasks = tasksObj => {
+  const transformTasks = useCallback(tasksObj => {
     const loadedTasks = [];
     for (const taskKey in tasksObj) {
       loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text })
     }
     setTasks(loadedTasks);
-  }
+  },
+    [])
 
-  useHttp({
-    url: 'https://custom-react-hooks-7c2be-default-rtdb.firebaseio.com/tasks.json'
-  });
+  const { isLoading, error, sendRequest: fetchTasks } = useHttp(transformTasks);
+
 
   useEffect(() => {
-    fetchTasks();
-  }, []);
+    fetchTasks({ url: 'https://custom-react-hooks-7c2be-default-rtdb.firebaseio.com/tasks.json' });
+  }, [fetchTasks]);
 
   const taskAddHandler = (task) => {
     setTasks((prevTasks) => prevTasks.concat(task));
--- project-10/src/hooks/use-http.js
+++ project-10/src/hooks/use-http.js
@@ -1,10 +1,10 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 
-const useHttp = (requestConfig, applyData) => {
+const useHttp = (applyData) => {
     const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState(null);
 
-    const sendRequest = async (taskText) => {
+    const sendRequest = useCallback(async (requestConfig) => {
         setIsLoading(true);
         setError(null);
         try {
@@ -26,7 +26,7 @@ const useHttp = (requestConfig, applyData) => {
             setError(err.message || 'Something went wrong!');
         }
         setIsLoading(false);
-    };
+    }, [applyData])
 
     return {
         isLoading: isLoading,
