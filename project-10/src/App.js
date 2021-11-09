249af633b87d4ff8571ba95d06254bc9aa7ca2db
unknown <lpsk5713@gmail.com>
2021-11-09 20:22:39 +0530

    Custom Component for HTTP Request Response Handling

--- project-10/src/App.js
+++ project-10/src/App.js
@@ -13,7 +13,7 @@ function App() {
     setError(null);
     try {
       const response = await fetch(
-        'https://react-http-6b4a6.firebaseio.com/tasks.json'
+        'https://custom-react-hooks-7c2be-default-rtdb.firebaseio.com/tasks.json'
       );
 
       if (!response.ok) {
--- project-10/src/components/NewTask/NewTask.js
+++ project-10/src/components/NewTask/NewTask.js
@@ -12,7 +12,7 @@ const NewTask = (props) => {
     setError(null);
     try {
       const response = await fetch(
-        'https://react-http-6b4a6.firebaseio.com/tasks.json',
+        'https://custom-react-hooks-7c2be-default-rtdb.firebaseio.com/tasks.json',
         {
           method: 'POST',
           body: JSON.stringify({ text: taskText }),
