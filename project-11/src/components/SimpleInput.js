55834face977014e23c5d7365c1512906cb251dd
unknown <lpsk5713@gmail.com>
2021-11-10 01:42:37 +0530

    State and Refs used ion Form

---
+++ project-11/src/components/SimpleInput.js
@@ -0,0 +1,29 @@
+import { useRef, useState } from "react";
+
+const SimpleInput = (props) => {
+  const nameInputRef = useRef();
+  const [enteredName, setEnteredName] = useState('');
+
+  const nameInputChangeHandler = (event) => {
+    setEnteredName(event.target.value);
+  }
+
+  const formSubmissionHandler = (event) => {
+    event.preventDefault();
+    const enteredValue = nameInputRef.current.value;
+  }
+
+  return (
+    <form onSubmit={formSubmissionHandler}>
+      <div className='form-control'>
+        <label htmlFor='name'>Your Name</label>
+        <input ref={nameInputRef} type='text' id='name' onChange={nameInputChangeHandler} />
+      </div>
+      <div className="form-actions">
+        <button>Submit</button>
+      </div>
+    </form>
+  );
+};
+
+export default SimpleInput;
