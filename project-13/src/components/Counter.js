e6dd484d5a2f4c7b703b8160419eee7d5c790215
unknown <lpsk5713@gmail.com>
2021-11-11 03:59:58 +0530

    Redux store created and applied

--- project-13/src/components/Counter.js
+++ project-13/src/components/Counter.js
@@ -1,12 +1,34 @@
 import classes from './Counter.module.css';
+import { useSelector, useDispatch } from 'react-redux';
 
 const Counter = () => {
-  const toggleCounterHandler = () => {};
+  const dispatch = useDispatch();
+  const counter = useSelector(state => state.counter);
+  const toggleCounterHandler = () => {
+    dispatch({ type: 'toggle' })
+  };
+  const show = useSelector(state => state.showCounter);
+
+  const incrementHandler = () => {
+    dispatch({ type: 'increment' });
+  }
+  const decrementHandler = () => {
+    dispatch({ type: 'decrement' });
+  }
+
+  const increaseHandler = () => {
+    dispatch({ type: 'toggle' })
+  }
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      {show && <div className={classes.value}>{counter}</div>}
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={increaseHandler}>Increase by 5</button>
+        <button onClick={decrementHandler}>Decrement</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
--- project-13/src/store/index.js
+++ project-13/src/store/index.js
@@ -1,14 +1,34 @@
 import { createStore } from 'redux';
 
-const counterReducer = (state = {
-    counter: 0
-}, action) => {
+const initialState = { counter: 0, showCounter: true };
+
+const counterReducer = (state = initialState, action) => {
     if (action.type === 'increment') {
-        return { counter: state.counter + 1 }
+        return {
+            counter: state.counter + 1,
+            showCounter: state.showCounter
+        }
     }
 
     if (action.type === 'decrement') {
-        return { counter: state.counter - 1 }
+        return {
+            counter: state.counter - 1,
+            showCounter: state.showCounter
+        }
+    }
+
+    if (action.type === 'increase') {
+        return {
+            counter: state.counter + action.amount,
+            showCounter: state.showCounter
+        }
+    }
+
+    if (action.type === "toggle") {
+        return {
+            showCounter: !state.showCounter,
+            counter: state.counter
+        }
     }
     return state;
 }
