a37f2869562038abebd1a5d7f4ddff397f3a761f
unknown <lpsk5713@gmail.com>
2021-11-12 01:18:05 +0530

    Basic Redux Completed

--- project-13/src/App.js
+++ project-13/src/App.js
@@ -2,12 +2,17 @@ import Counter from './components/Counter';
 import { Fragment } from 'react';
 import Header from './components/Header'
 import Auth from './components/Auth'
+import { useSelector } from 'react-redux';
+import UserProfile from './components/UserProfile';
 
 function App() {
+  const isAuth = useSelector(state => state.auth.isAuthenticated);
+
   return (
     <Fragment>
       <Header />
-      <Auth />
+      {!isAuth && <Auth />}
+      {isAuth && <UserProfile />}
       <Counter />
     </Fragment>
   );
