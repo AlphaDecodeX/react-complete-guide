ee8774fa1a32b96aeb78645eef4992657f8aadb8
unknown <lpsk5713@gmail.com>
2021-11-19 02:31:39 +0530

    History Routing Done with Nested Routing

--- project-16/src/pages/QuoteDetail.js
+++ project-16/src/pages/QuoteDetail.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Fragment } from 'react';
-import { useParams, Route } from 'react-router-dom'
+import { useParams, Route, Link } from 'react-router-dom'
 import Comments from '../components/comments/Comments'
 import HighlightedQuote from "../components/quotes/HighlightedQuote"
 
@@ -20,6 +20,11 @@ function QuoteDetail() {
 
         <Fragment>
             <HighlightedQuote text={quote.text} author={quote.author} />
+            <Route path={`/quotes/${params.quoteId}`} exact>
+                <div className="centered">
+                    <Link className="btn--flat" to={`/quotes/${params.quoteId}/comments`}>Load Comments</Link>
+                </div>
+            </Route>
             <Route path={`/quotes/${params.quoteId}/comments`}>
                 <Comments />
             </Route>
