9efc4ae0964e204e1bf5073e8c28f5b87aa7e07c
unknown <lpsk5713@gmail.com>
2021-11-29 10:30:22 +0530

    TypeScript Basics Done

--- TypeScript/basics.ts
+++ TypeScript/basics.ts
@@ -55,3 +55,16 @@ function adds(a: number, b: number): number | string {
 function print(value: any) {
   console.log(value);
 }
+
+// Generics
+
+function insertAtBeginning<T>(array: T[], value: T) {
+  const newArray = [value, ...array];
+  return newArray;
+}
+
+const demoArray = [1, 2, 3];
+const updatedArray = insertAtBeginning(demoArray, -1);
+const stringArray = insertAtBeginning(["a", "b", "c"], "d");
+
+// updatedArray[0].split("");
